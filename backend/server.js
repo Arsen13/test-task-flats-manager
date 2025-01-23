9801b24a42357279f102ab165d7a0814f61d0d40
Kohut Arsen <kohut.arsen@gmail.com>
2025-01-23 16:16:47 +0200

    Create a basic express app

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/', flatRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
